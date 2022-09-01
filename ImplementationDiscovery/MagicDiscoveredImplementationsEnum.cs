e07fd539b12d67759f261565df4ad6a02402d47a
Max Bergman <maxbergman@outlook.com>
2022-09-01 15:05:45 +0200

    0.20.3: Updated MagicEnums package.

--- ImplementationDiscovery/MagicDiscoveredImplementationsEnum.cs
+++ ImplementationDiscovery/MagicDiscoveredImplementationsEnum.cs
@@ -21,24 +21,30 @@ public abstract record MagicDiscoveredImplementationsEnum<TSelf, TValue, TBaseTy
 	/// <returns>The newly created member.</returns>
 	/// <exception cref="InvalidOperationException">When a member with the same name already exists.</exception>
 	protected static TSelf CreateMember(TValue value) 
-		=> MagicEnumCore<TSelf, TValue>.CreateMember(valueCreator: () => value, memberCreator: () => new TSelf(), name: GetNameWithoutBacktick(value));
+		=> MagicEnumCore<TSelf, TValue>.CreateMember(
+			valueCreator: () => value, 
+			memberCreator: () => new TSelf(), 
+			name: GetNameWithoutBacktick(value.UninitializedInstance.GetType().Name));
 	
 	/// <summary>
 	/// Creates a new enum member and returns it or gets an existing member if one already exist of the same name.
 	/// </summary>
+	/// <param name="name">The name of the new member.</param>
 	/// <param name="value">The (newable) uninitialized object.</param>
 	/// <returns>The newly created member or an existing member with the same name.</returns>
-	public static TSelf GetOrCreateMember(TValue value) 
-		=> MagicEnumCore<TSelf, TValue>.GetOrCreateMember(valueCreator: () => value, memberCreator: () => new TSelf(), name: GetNameWithoutBacktick(value));
+	public static TSelf GetOrCreateMember(string name, TValue value) 
+		=> MagicEnumCore<TSelf, TValue>.GetOrCreateMember(
+			name: GetNameWithoutBacktick(name), 
+			valueCreator: () => value, 
+			memberCreator: () => new TSelf());
 	
 	/// <summary>
 	/// Get an enumerable over the uninitialized objects.
 	/// </summary>
 	public static IEnumerable<TBaseType> GetUninitializedObjects() => GetMembers().Select(member => member.Value.UninitializedInstance);
 
-	private static string GetNameWithoutBacktick(TValue value)
+	private static string GetNameWithoutBacktick(string name)
 	{
-		var name = value.UninitializedInstance.GetType().Name;
 		var index = name.IndexOf('`');
 		
 		return index == -1 
